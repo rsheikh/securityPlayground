c6265824d88ddd040ceb3d7ba70288cc6992cda1
Oliver Arroyo <o.t.arroyo@gmail.com>
2024-09-03 15:31:37 +0100

    feat: add welcomeAuthorisedUser method

--- src/main/java/com/northcoders/SecurityPlayground/controller/SecurityController.java
+++ src/main/java/com/northcoders/SecurityPlayground/controller/SecurityController.java
@@ -1,5 +1,10 @@
 package com.northcoders.SecurityPlayground.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,8 +19,11 @@ public class SecurityController {
     }
 
     @GetMapping("/protected/greeting")
-    public static String welcomeAuthorisedUser() {
-        return "Hiya User. Welcome to it all!";
+    public static ResponseEntity<String> welcomeAuthorisedUser(@AuthenticationPrincipal OAuth2User principal) {
+        String username = principal.getName();
+        String name = principal.getAttribute("name");
+
+        return new ResponseEntity<>("Hiya" + name + " your username is: " + username + ". Welcome to it all!", HttpStatus.OK);
     }
 
 }
