43c5cde8c18961dddd7af12db0638763c53ccf12
Oliver Arroyo <o.t.arroyo@gmail.com>
2024-09-03 12:02:35 +0100

    feat: add filterChain method

---
+++ src/main/java/com/northcoders/SecurityPlayground/security/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.northcoders.SecurityPlayground.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((auth) ->
+            auth.requestMatchers("/api/v1/open/greeting").permitAll()
+                .requestMatchers("/api/v1/protected/greeting").authenticated())
+            .httpBasic(Customizer.withDefaults())
+            .formLogin(Customizer.withDefaults());
+        return http.build();
+    }
+
+}
