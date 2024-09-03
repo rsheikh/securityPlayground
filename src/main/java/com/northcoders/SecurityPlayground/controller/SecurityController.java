1eba3870d79602f3eb1e7c0a7bb849210810dbf4
rsheikh <red.sheikh@gmail.com>
2024-09-03 10:52:43 +0100

    feat: add securityController with 2 endpoints

---
+++ src/main/java/com/northcoders/SecurityPlayground/controller/SecurityController.java
@@ -0,0 +1,21 @@
+package com.northcoders.SecurityPlayground.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/vi")
+public class SecurityController {
+
+    @GetMapping("/open/greeting")
+    public static String welcomeUser() {
+       return "Welcome User";
+    }
+
+    @GetMapping("/protected/greeting")
+    public static String welcomeAuthorisedUser() {
+        return "Hiya User. Welcome to it all!";
+    }
+
+}
