4cc5b7478625d17ea78cf9d2a4183dc170439e64
Oliver Arroyo <o.t.arroyo@gmail.com>
2024-09-03 12:03:11 +0100

    refactor: change root api endpoint

--- src/main/java/com/northcoders/SecurityPlayground/controller/SecurityController.java
+++ src/main/java/com/northcoders/SecurityPlayground/controller/SecurityController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/vi")
+@RequestMapping("/api/v1")
 public class SecurityController {
 
     @GetMapping("/open/greeting")
